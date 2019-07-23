c3eab31d94a55e3a168c79cff51d6888a873d802
Tao Zhu <bill_zt@sina.com>
2019-07-23 13:48:08 +0800

    Split to sub-command

--- primerserver2/cmd/primertool.py
+++ primerserver2/cmd/primertool.py
@@ -29,33 +29,65 @@ def error(msg, judge):
     raise Exception(msg)
 
 def make_args():
-    parser = argparse.ArgumentParser(description='PrimerServer2: a high-throughput primer \
-        design and specificity-checking platform', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('query', help='query file. (STDIN is acceptable)', type=argparse.FileType('r'))
-    parser.add_argument('template', help='template file in FASTA format')
+    parser = argparse.ArgumentParser(description='primertool: the command-line version of PrimerServer2', \
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--no-specificity', help="Don't check specificity; Only design primers", action='store_true')
-    group.add_argument('--only-specificity', help="Only check specificity and skip primer designs", action='store_true')
-    parser.add_argument('--type', choices=['SEQUENCE_TARGET', 'SEQUENCE_INCLUDED_REGION', 'FORCE_END'],\
+    # parent arguments
+    # These arguments are used by all the three modes
+    parent_parser_all = argparse.ArgumentParser(add_help=False)
+    group_io = parent_parser_all.add_argument_group('Input (REQUIRED)')
+    group_io.add_argument('query', help='query file. (STDIN is acceptable)', \
+        type=argparse.FileType('r'))
+    group_io.add_argument('templates', help='template file in FASTA format. \
+        Allowing multiple files (separated by comma), where the first one is used to design primers and/or order the primer \
+            specificity')
+
+    group_all = parent_parser_all.add_argument_group('Overall Setttings')
+    group_all.add_argument('-r', '--primer-num-retain', type=int, help='The maximum number of primers to return in each site.', \
+        default=10)
+    group_all.add_argument('-p', '--cpu', type=int, help='Used CPU number.', default=2)
+    group_all.add_argument('--json-debug', help="Output debug information in JSON mode", action='store_true')
+    group_all.add_argument('-o', '--out', help="Output primers in JSON format. (Default is STDIN)", type=argparse.FileType('w'))
+    group_all.add_argument('-t', '--tsv', help="Output primers in TSV format", type=argparse.FileType('w'))
+
+    # These arguments are used by design and full
+    parent_parser_design = argparse.ArgumentParser(add_help=False)
+    group_design = parent_parser_design.add_argument_group('Design Primers')
+    group_design.add_argument('--type', choices=['SEQUENCE_TARGET', 'SEQUENCE_INCLUDED_REGION', 'FORCE_END'],\
         help='designing primer types', default='SEQUENCE_TARGET')
-    parser.add_argument('-p', '--cpu', type=int, help='Used CPU number.', default=2)
-    parser.add_argument('-d', '--Tm-diff', type=int, \
+    
+    # These arguments are used by check and full
+    parent_parser_check = argparse.ArgumentParser(add_help=False)
+    group_check = parent_parser_check.add_argument_group('Check Specificity')
+    group_check.add_argument('--Tm-diff', type=int, \
         help='The mininum melting temperature (in ℃) suggested to produce off-target amplicon. Suggest >10', default=20)
-    parser.add_argument('-3', '--use-3-end', help='If turned on, primer pairs having at least one mismatch at the 3 end\
+    group_check.add_argument('-3', '--use-3-end', help='If turned on, primer pairs having at least one mismatch at the 3 end\
         position with templates would not be considered to produce off-target amplicon, even if their melting temperatures \
             are high. Turn on this would find more candidate primers, but might also have more false positives\
                 ', action='store_true')
-    parser.add_argument('-l', '--checking-size-min', type=int, help='Lower limit of the checking amplicon size range (bp).', \
+    group_check.add_argument('-l', '--checking-size-min', type=int, help='Lower limit of the checking amplicon size range (bp).', \
         default=70)
-    parser.add_argument('-u', '--checking-size-max', type=int, help='Upper limit of the checking amplicon size range (bp).', \
+    group_check.add_argument('-u', '--checking-size-max', type=int, help='Upper limit of the checking amplicon size range (bp).', \
         default=1000)
-    parser.add_argument('-r', '--primer-num-retain', type=int, help='The maximum number of primers for each site to return.', \
-        default=10)
-    parser.add_argument('-a', '--report-amplicon-seqs', help="Get amplicon seqs (might be slow)", action='store_true')
-    parser.add_argument('--json-debug', help="Output debug information in JSON mode", action='store_true')
-    parser.add_argument('-o', '--out', help="Output primers in JSON format. (Default is STDIN)", type=argparse.FileType('w'))
-    parser.add_argument('-t', '--tsv', help="Output primers in TSV format", type=argparse.FileType('w'))
+    group_check.add_argument('-a', '--report-amplicon-seqs', help="Get amplicon seqs (might be slow)", action='store_true')
+
+    # sub commands
+    subparsers = parser.add_subparsers(help='Sub commands: running mode')
+    subparsers.required = True
+    subparsers.dest = 'run_mode'
+    # sub commands: full (design primers and check specificity)
+    parser_full = subparsers.add_parser('full', help='design primers, check their specificity and output specific ones', \
+        parents=[parent_parser_all, parent_parser_design, parent_parser_check], \
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # sub commands: design (design primers only, don't check specificity)
+    parser_design = subparsers.add_parser('design', help="design primers only, don't check specificity", \
+        parents=[parent_parser_all, parent_parser_design], formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # sub commands: check (check specificity only, skip the design step)
+    parser_check = subparsers.add_parser('check', help='check specificity only, skip the design step', \
+        parents=[parent_parser_all, parent_parser_check], formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
     args = parser.parse_args()
     return args
 
@@ -73,26 +105,27 @@ def check_environments(args):
     if shutil.which('makeblastdb') is None:
         error('No NCBI-BLAST+ (makeblastdb) detected in your system', args.json_debug)
 
-def check_template(args):
-    if os.path.isfile(args.template) is False:
-        error(f'File not found: {args.template}', args.json_debug)
-    if os.path.isfile(args.template+'.fai') is False:
-        code = os.system(f'samtools faidx {args.template} 2>/dev/null')
-        if code != 0:
-            error(f'File {args.template} cannot be indexed by samtools faidx. Perhaps it is not in FASTA format', args.json_debug)
+def check_templates(args):
+    for template in args.templates.split(','):
+        if os.path.isfile(template) is False:
+            error(f'File not found: {template}', args.json_debug)
+        if os.path.isfile(template+'.fai') is False:
+            code = os.system(f'samtools faidx {template} 2>/dev/null')
+            if code != 0:
+                error(f'File {template} cannot be indexed by samtools faidx. Perhaps it is not in FASTA format', args.json_debug)
 
 def run(args):
     ###################  Design primers ###################
     query_string = args.query.read()
-    if args.only_specificity is True:
+    dbs = args.templates.split(',')
+    if args.run_mode=='check':
         primers = make_primers.make_primers(query=query_string)
     else:
-        sites = make_sites.build(query=query_string, template_file=args.template, primer_type=args.type)
+        sites = make_sites.build(query=query_string, template_file=dbs[0], primer_type=args.type)
         primers = design_primer.multiple(sites, cpu=args.cpu)
 
     ###################  Checking specificity  #############
-    dbs = [args.template]
-    if args.no_specificity is False:
+    if args.run_mode!='design':
         primers = run_blast.run_blast_parallel(primers=primers, dbs=dbs, cpu=args.cpu,\
             checking_size_max=args.checking_size_max, checking_size_min=args.checking_size_min, \
                 report_amplicon_seq=args.report_amplicon_seqs, Tm_diff=args.Tm_diff, use_3_end=args.use_3_end)
@@ -105,13 +138,12 @@ def run(args):
         print(json.dumps(primers, indent=4))
 
     if args.tsv is not None:
-        if args.no_specificity is False:
-            output.after_check(primers, dbs, file=args.tsv)
+        output.after_check(primers, dbs, file=args.tsv)
 
 def main():
     args = make_args()
     check_environments(args)
-    check_template(args)
+    check_templates(args)
     run(args)
 
 if __name__ == "__main__":
