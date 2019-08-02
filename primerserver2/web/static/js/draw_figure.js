eb2fe2e779f8e24cfb2e6e55df952c1ff00b2c3a
Tao Zhu <bill_zt@sina.com>
2019-08-02 13:13:24 +0800

    web draft

---
+++ primerserver2/web/static/js/draw_figure.js
@@ -0,0 +1,174 @@
+/***************** Complex functions to display Figure after showing Panel, used after the server return results */
+function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
+}
+function getMinOfArray(numArray) {
+    return Math.min.apply(null, numArray);
+}
+function GenerateGraph(el, specificity) {
+    // empty the element
+    el.find('.PrimerFigure').html('');
+    
+    // svg
+    var svg = d3.select(el.find('.PrimerFigure')[0])
+                .append('svg')
+                .attr('width', '100%');
+    svg.append('style').text('.axis path,.axis line{fill: none;stroke: blue;stroke-width: 2px;shape-rendering: crispEdges;} '
+        +'.axis text{font-size:11px;} .primerLabel{font-size:11px;} .primerUniqueLabel{font-size:11px;font-weight:bold;}');
+    
+    // primers regions
+    var primers = el.find('.list-group-item-primer');
+    var primers2region = new Object;
+    var primers2hit = new Object;
+    var allPoses = new Array;
+    for (var i=0; i<primers.length; i++) {
+        var id = $(primers[i]).find('.list-group-item-heading').attr('id');
+        var primer_left = $(primers[i]).find('.primer-left-region');
+        var primer_right = $(primers[i]).find('.primer-right-region');
+        var region_1 = $(primer_left[0]).html().split('-');
+        var region_2 = $(primer_right[0]).html().split('-');
+        primers2region[id] = [region_1, region_2];
+        Array.prototype.push.apply(allPoses, region_1);
+        Array.prototype.push.apply(allPoses, region_2);
+        if (specificity==true) {
+            var hitNum = $(primers[i]).find('.hit-num').data('hit');
+            primers2hit[id] = hitNum;
+        }
+    }
+
+    // axis
+    var axisStart;
+    var axisEnd;
+    var targetPos = el.prev().find('.site-detail').data('pos');
+    var targetLen = el.prev().find('.site-detail').data('length');
+    var region_type = $('[name="region_type"]').val();
+    if (region_type=='SEQUENCE_INCLUDED_REGION') {
+        axisStart = targetPos-Math.round(targetLen/5)>0 ? targetPos-Math.round(targetLen/5) : 1;
+        axisEnd = targetPos+targetLen+Math.round(targetLen/5);
+    }
+    else if (region_type=='SEQUENCE_TARGET' || region_type=='FORCE_END') {
+        axisStart = getMinOfArray(allPoses);
+        axisEnd = getMaxOfArray(allPoses);
+    }
+    var axisScale = d3.scaleLinear().domain([axisStart, axisEnd]).range([0, 1000]);
+    var axis = d3.axisTop().scale(axisScale).ticks(10);
+    svg.append('g').attr('class', 'axis').call(axis); // axis: translate(x,y) is no longer needed as PanZoom can do it
+    
+    // Text
+    var template = el.prev().find('.site-detail').data('seq');
+    svg.append('text').attr('x','0').attr('y','-30').text('Template '+template).attr('font-size', '120%');
+
+    // target region
+    var rectHight = 60;
+    svg.append('rect').attr('x', axisScale(targetPos))  
+       .attr('y', -rectHight/2)  // axis:y-rect:height/2
+       .attr('width', axisScale(targetPos+targetLen)-axisScale(targetPos)).attr('height', rectHight)
+       .attr("fill", "none").attr('stroke', 'red').attr('stroke-width', '3');
+    
+    // Primer Group
+    var colorScale = d3.scaleLinear().domain([1, 100]).range([0, 32]);
+    function AddPrimer(LprimerStart, LprimerEnd, RprimerStart, RprimerEnd, i, h, id) {
+        var primerGroup = svg.append('a').attr('xlink:href','#'+id).attr('class', 'primerGroup')
+                        .attr('title', 'Primer '+i).append('g');
+        var baseY = rectHight+30*(i-1);
+        var lineFunction = d3.line()
+            .x(function(d) { return Math.round(axisScale(d.x)); })
+            .y(function(d) { return d.y; });
+        var color = 'rgb('+Math.round(colorScale(h)*8)+','+Math.round(colorScale(h)*8)+','+Math.round(colorScale(h)*8)+')';
+        
+        // Left Primer
+        var Llength = LprimerEnd-LprimerStart+1;
+        
+        var LlineData = [ { "x": LprimerStart, "y": baseY-5},  { "x": LprimerStart+Math.round(Llength/3*2), "y": baseY-5},
+                         { "x": LprimerStart+Math.round(Llength/3*2), "y": baseY-10}, {"x": LprimerEnd, "y": baseY},
+                         { "x": LprimerStart+Math.round(Llength/3*2), "y": baseY+10}, {"x": LprimerStart+Math.round(Llength/3*2), "y": baseY+5},
+                         { "x": LprimerStart, "y": baseY+5},  { "x": LprimerStart, "y": baseY-5}];
+        
+        primerGroup.append('path').attr('d', lineFunction(LlineData)).attr("fill", color).attr('stroke', color);
+        
+        
+        // Right Primer
+        var Rlength = RprimerEnd-RprimerStart+1;
+        
+        var RlineData = [ {"x": RprimerStart, "y": baseY}, {"x": RprimerStart+Math.round(Rlength/3*1), "y": baseY-10},
+                          {"x": RprimerStart+Math.round(Rlength/3*1), "y": baseY-5}, {"x": RprimerEnd, "y": baseY-5},
+                          {"x": RprimerEnd, "y": baseY+5}, {"x": RprimerStart+Math.round(Rlength/3*1), "y": baseY+5},
+                          {"x": RprimerStart+Math.round(Rlength/3*1), "y": baseY+10}, {"x": RprimerStart, "y": baseY}];
+        primerGroup.append('path').attr('d', lineFunction(RlineData)).attr("fill", color).attr('stroke', color);
+        
+        // Center Line
+        var LineData = [{"x": LprimerEnd, "y": baseY}, {"x": RprimerStart, "y": baseY}];
+        primerGroup.append('path').attr('d', lineFunction(LineData)).attr("fill", color).attr('stroke', color);
+        
+        // Text
+        if (h==1) {
+            primerLabel = '[Primer'+i+'] '+h+' Amplicon';
+            primerGroup.append('text').attr("x", axisScale(LprimerStart)-120)
+            .attr("y", baseY+5).attr('class', 'primerUniqueLabel').attr('fill', 'red').text(primerLabel);
+        }
+        else if (h>1) {
+            primerLabel = '[Primer'+i+'] '+h+' Amplicons';
+            primerGroup.append('text').attr("x", axisScale(LprimerStart)-120)
+            .attr("y", baseY+5).attr('class', 'primerLabel').text(primerLabel);
+        }
+        else {
+            primerLabel = '[Primer'+i+'] ';
+            primerGroup.append('text').attr("x", axisScale(LprimerStart)-120)
+            .attr("y", baseY+5).attr('class', 'primerLabel').text(primerLabel);
+        }
+    }
+
+    var primerRank = 1;
+    for ( id in primers2region) {
+        var LprimerStart=primers2region[id][0][0];
+        var LprimerEnd=primers2region[id][0][1];
+        var RprimerStart=primers2region[id][1][0];
+        var RprimerEnd=primers2region[id][1][1];
+        var h = specificity==true ? primers2hit[id] : -1;
+        AddPrimer(LprimerStart*1, LprimerEnd*1, RprimerStart*1, RprimerEnd*1, primerRank*1, h*1, id);
+        primerRank++;
+        //break;
+    }
+    
+    // extend svg height if there are too many primers
+    if (primerRank>3) {
+        svg.attr('height', (primerRank-3)*30+150);
+    }
+
+    // Pan and Zoom
+    if ($('.PrimerFigure svg').length>0) {
+        var zoomObj = svgPanZoom('.PrimerFigure svg');
+        if (el.find('.PrimerFigureControl').length==0) {
+            el.find('.PrimerFigure').before('<div class="PrimerFigureControl btn-group">'
+                +'<button type="button" class="btn btn-default zoom-in" title="Zoom in" data-toggle="tooltip"><i class="fa fa-search-plus"></i></button>'
+                +'<button type="button" class="btn btn-default zoom-out" title="Zoom out" data-toggle="tooltip"><i class="fa fa-search-minus"></i></button>'
+                +'<button type="button" class="btn btn-default zoom-reset" title="Reset" data-toggle="tooltip"><i class="fa fa-refresh"></i></button>'
+                +'<button type="button" class="btn btn-default svg-download" title="Download SVG" data-toggle="tooltip"><i class="fa fa-download"></i></button>'
+                +'</div>');                
+        }
+        $('[data-toggle="tooltip"]').tooltip({html: true});
+        $(window).resize(function(){
+            zoomObj.resize();
+            zoomObj.fit();
+            zoomObj.center();
+        });
+        $('.zoom-in').click(function(){
+            zoomObj.zoomIn();
+        });
+        $('.zoom-out').click(function(){
+            zoomObj.zoomOut();
+        });
+        $('.zoom-reset').click(function(){
+            zoomObj.resetZoom().resetPan();
+        });
+        $('.svg-download').click(function(){
+            var download_text = '<svg width="100%" xmlns="http://www.w3.org/2000/svg" '
+            +'xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events"> '
+            + $('.PrimerFigure svg').html() + '</svg>';
+            var blob = new Blob([download_text], {type: "text/svg;charset=utf-8"});
+            saveAs(blob, "primer.svg"); 
+        });
+    }
+}
+
+/***************** Complex functions finished  ***********************************************************/
---
+++ primerserver2/web/static/js/generate_html_result.js
@@ -0,0 +1,162 @@
+function generate_html_result(selected_dbs, db_name_change, data){
+    $('#primers-result').html('');
+    var result_data = data; 
+    var site_rank = 0;
+    for (site_id in result_data) {
+        site_rank += 1;
+
+        // site
+        var raw_html_site = $('#primers-result-template-site').html();
+        $('#primers-result-template-site a.collapsed').attr('href', '#site-'+site_rank)
+            .html('Site '+site_rank+' <span class="caret"></span>');
+        $('#primers-result-template-site .site-detail').html('Site: '+site_id);
+        if (mode!='check') {
+            let i = site_id.split('-');
+            site_seq = i.slice(0,-2).join('-');
+            site_pos = i[i.length-2];
+            site_len = i[i.length-1];
+            $('#primers-result-template-site .site-detail').attr('data-seq', site_seq).attr('data-pos', site_pos)
+                .attr('data-length', site_len);
+        }
+        var primer_num = 0;
+        if ('PRIMER_PAIR_NUM_RETURNED_FINAL' in result_data[site_id]) {
+            primer_num = result_data[site_id]['PRIMER_PAIR_NUM_RETURNED_FINAL']
+        }
+        else {
+            primer_num = result_data[site_id]['PRIMER_PAIR_NUM_RETURNED']
+        }
+        $('#primers-result-template-site .site-primer-num').html(primer_num);
+        var main_db = selected_dbs.split(',')[0];
+        if (main_db in result_data[site_id]) {
+            var least_amplicon_num_in_main_db = result_data[site_id][main_db]['PRIMER_PAIR_'
+                +result_data[site_id]['PRIMER_PAIR_AMPLICON_NUM_RANK_0']+'_AMPLICONS'].length;
+            if (least_amplicon_num_in_main_db==1) {
+                $('#primers-result-template-site .row-site-info')
+                    .append('<div class="col-md-1"><span class="glyphicon glyphicon-ok"></span></div>');
+            }
+        }
+        $('#primers-result-template-site .panel-site').attr('id', 'site-'+site_rank);
+        if (site_rank==1) {
+            $('#site-1').addClass('in');
+        }
+        $('#primers-result').append($('#primers-result-template-site').html());
+        $('#primers-result-template-site').html(raw_html_site);
+
+        // primer
+        for (var i=0; i<primer_num; i++) {
+            var raw_html_primer = $('#primers-result-template-primer').html();
+            var primer_rank = i+1;
+            var raw_rank = 0;
+            if ('PRIMER_PAIR_NUM_RETURNED_FINAL' in result_data[site_id]) {
+                raw_rank = result_data[site_id]['PRIMER_PAIR_AMPLICON_NUM_RANK_'+i];
+            }
+            else {
+                raw_rank = i;
+            }
+            var amplicon_num_in_main_db = 0;
+            if (main_db in result_data[site_id]) {
+                amplicon_num_in_main_db = result_data[site_id][main_db]['PRIMER_PAIR_'+raw_rank+'_AMPLICONS'].length;
+                if (amplicon_num_in_main_db==1) {
+                    $('#primers-result-template-primer .list-group-item-primer').addClass('list-group-item-success')
+                }
+            }
+
+            $('#primers-result-template-primer h4').attr('id', 'Site'+site_rank+'-Primer'+primer_rank)
+                .html('Primer '+primer_rank);
+            
+            var p_start = result_data[site_id]['PRIMER_LEFT_'+raw_rank][0]+1;
+            p_start = p_start==0 ? '' : p_start;
+            var p_len = result_data[site_id]['PRIMER_LEFT_'+raw_rank][1];
+            var p_end = p_start=='' ? '' : p_start+p_len-1;
+            $('#primers-result-template-primer .primer-seq-detail')
+                .append('<tr><th>Left Primer</th>'
+                    +'<td><span class="monospace-style">'+result_data[site_id]['PRIMER_LEFT_'+raw_rank+'_SEQUENCE']+'</span></td>'
+                    +'<td>'+p_len+'</td>'
+                    +'<td class="primer-left-region">'+p_start+'-'+p_end+'</td>'
+                    +'<td>'+result_data[site_id]['PRIMER_LEFT_'+raw_rank+'_TM'].toFixed(1)+'</td>'
+                    +'<td>'+result_data[site_id]['PRIMER_LEFT_'+raw_rank+'_GC_PERCENT'].toFixed(1)+'</td>'
+                    +'</tr>')
+
+            p_start = result_data[site_id]['PRIMER_RIGHT_'+raw_rank][0]+1;
+            p_start = p_start==0 ? '' : p_start;
+            p_len = result_data[site_id]['PRIMER_RIGHT_'+raw_rank][1];
+            p_end = p_start=='' ? '' : p_start+p_len-1;
+            $('#primers-result-template-primer .primer-seq-detail')
+                .append('<tr><th>Right Primer</th>'
+                    +'<td><span class="monospace-style">'+result_data[site_id]['PRIMER_RIGHT_'+raw_rank+'_SEQUENCE']+'</span></td>'
+                    +'<td>'+p_len+'</td>'
+                    +'<td class="primer-right-region">'+p_start+'-'+p_end+'</td>'
+                    +'<td>'+result_data[site_id]['PRIMER_RIGHT_'+raw_rank+'_TM'].toFixed(1)+'</td>'
+                    +'<td>'+result_data[site_id]['PRIMER_RIGHT_'+raw_rank+'_GC_PERCENT'].toFixed(1)+'</td>'
+                    +'</tr>')
+            
+            var product_size = result_data[site_id]['PRIMER_PAIR_'+raw_rank+'_PRODUCT_SIZE'];
+            product_size = product_size==-1? '-' : product_size;
+            $('#primers-result-template-primer .primer-seq-detail')
+                .append('<tr><th>Product Size</th><td colspan="5">'+product_size+' bp</td></tr>');
+            var penalty = result_data[site_id]['PRIMER_PAIR_'+raw_rank+'_PENALTY'].toFixed(2);
+            penalty = penalty==0? '-' : penalty;
+            $('#primers-result-template-primer .primer-seq-detail')
+                .append('<tr><th>Penalty</th><td colspan="5">'+penalty+'</td></tr>');
+
+
+            // dbs
+            var db_rank = 0;
+            for (db of selected_dbs.split(',')) {
+                if (db in result_data[site_id] == false) {  // if no specificity check
+                    $('#primers-result-template-primer .amplicons_table').addClass('hidden');
+                    continue;
+                }
+                db_desc = db_name_change[db]
+                if (db_rank==0) {
+                    $('#primers-result-template-primer .database-list').append('<th>Database: '
+                        +db_desc+' <span class="glyphicon glyphicon-star"></span></th>')
+                }
+                else {
+                    $('#primers-result-template-primer .database-list').append('<th>Database: '
+                        +db_desc+'</th>')
+                }
+                // amplicons
+                if ('PRIMER_PAIR_'+raw_rank+'_AMPLICONS' in result_data[site_id][db]) {
+                    amplicons = result_data[site_id][db]['PRIMER_PAIR_'+raw_rank+'_AMPLICONS'];
+                } 
+                else {
+                    amplicons = [];
+                }
+                amplicon_num = amplicons.length;
+                $('#primers-result-template-primer .amplicons_number').append('<td class="hit-num" data-hit="'
+                    +amplicon_num+'">Amplicon Number: '+amplicon_num+'</td>');
+                $('#primers-result-template-primer .amplicons_region')
+                    .append('<td><ul class="list-group list-group-amplicons-'+db_rank+'"></ul></td>');
+                var output_amplicon_num = 0;
+                for (amplicon of amplicons) {
+                    output_amplicon_num++;
+                    if (amplicon_num==1) {
+                        $('#primers-result-template-primer .list-group-amplicons-'+db_rank)
+                            .append('<li class="list-group-item list-group-item-success">'+amplicon['region']
+                                +', '+amplicon['product_size']+' bp</li>');
+                    }
+                    else {
+                        $('#primers-result-template-primer .list-group-amplicons-'+db_rank)
+                            .append('<li class="list-group-item">'+amplicon['region']+', '+amplicon['product_size']+' bp</li>');
+                    }
+                    if (output_amplicon_num==3) {
+                        if (amplicon_num==1) {
+                            $('#primers-result-template-primer .list-group-amplicons-'+db_rank)
+                                .append('<li class="list-group-item list-group-item-success">...</li>');
+                        }
+                        else {
+                            $('#primers-result-template-primer .list-group-amplicons-'+db_rank)
+                                .append('<li class="list-group-item">...</li>');
+                        }
+                        break;
+                    }
+                }
+                db_rank++;
+            }
+
+            $('#site-'+site_rank).find('.list-group-primers').append($('#primers-result-template-primer').html());
+            $('#primers-result-template-primer').html(raw_html_primer);
+        } 
+    }
+}
